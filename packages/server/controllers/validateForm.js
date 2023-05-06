187c55de8a6c720b529f7718c260f3b2a14ac80c
jorgeasmz <jorge.eduardo.arias@correounivalle.edu.co>
2023-05-06 02:08:33 -0500

    Added rate limiting with Redis

--- packages/server/controllers/validateForm.js
+++ packages/server/controllers/validateForm.js
@@ -1,15 +1,20 @@
-const {formSchema} = require("@whatsapp-clone/common")
+const { formSchema } = require("@whatsapp-clone/common");
 
-const validateForm = (req,res) => {
-    const formData = req.body;
-    formSchema.validate(formData).catch(err => {
-        res.status(422).send();
-        console.log(err.errors);
-    }).then(valid => {
-        if (valid) {
-            console.log("form is good");
-        }
-    });
-}
+const validateForm = (req, res, next) => {
+	const formData = req.body;
+	formSchema
+		.validate(formData)
+		.catch(() => {
+			res.status(422).send();
+		})
+		.then((valid) => {
+			if (valid) {
+				console.log("form is good");
+				next();
+			} else {
+				res.status(422).send();
+			}
+		});
+};
 
 module.exports = validateForm;
